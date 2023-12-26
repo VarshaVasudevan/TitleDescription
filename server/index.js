2abf49ff97a62087bb340aeea48dd87c0a3e4bb1
Varsha Vasudevan <varshavasudevan2023@gmail.com>
2023-12-26 19:14:07 +0530

    server

--- server/index.js
+++ server/index.js
@@ -10,23 +10,15 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
-const allowedOrigins = ['https://658ad417215c8c9b3b6c9c20--adorable-kelpie-e03d5a.netlify.app/'];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  };
   
 app.use(cors(corsOptions));
-//app.use(cors());
 app.use("/",router)
 app.use(express.static('uploads'));
 
